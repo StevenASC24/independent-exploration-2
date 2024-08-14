c54170e02a44f2a5d667fe0996c27058d54682bb
StevenASC24 <Stevendlt9@gmail.com>
2024-08-14 16:10:51 -0400

    first commit

--- intervals/script.js
+++ intervals/script.js
@@ -3,3 +3,11 @@ let time = 0;
 // reference to the <span> containing the number
 let counterElement = document.getElementById("time");
 
+let clock = 0
+
+function addtime() {
+    clock++
+    counterElement.innerHTML = clock
+}
+
+const intervalId = setInterval(addtime, 1000);
